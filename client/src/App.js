8caffd12fd236435ea26e7a5f8785566c3a6fd49
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-06 16:55:47 +0200

    feat: redux connect store with app, demo action, and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { actionSimple } from './actions/action.simple';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  buttonClicked = (event) => {
+    this.props.actionSimple();
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,11 +21,21 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.buttonClicked}>Test redux action</button>
         </p>
+
+        <p>{JSON.stringify(this.props)}</p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchtoProps = dispatch => ({
+  actionSimple: () => dispatch(actionSimple())
+})
+
+export default connect(mapStateToProps, mapDispatchtoProps)(App);
