c0ce297b40e942f29ed7ea1c5787f813b9fcbd27
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-06 18:27:06 +0200

    feat: dependency react-helmet for dynamic head meta tags

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
 import { actionSimple } from './actions/action.simple';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -15,17 +15,36 @@ class App extends Component {
 
   render() {
     return (
+      <div>
+        <Helmet>
+          <title>React App in Title</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Helmet>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <p className="App-title">Welcome to App</p>
         </header>
-        <p className="App-intro">
-          <button onClick={this.buttonClicked}>Test redux action</button>
-        </p>
+        <section>
+          <h1>About privacy and Location Services in iOS 8 and later</h1>
+          <p>
+            Learn about services in iOS 8 and later that protect your private information, including your location on your iPhone, iPad, and iPod touch.
+          </p>
+        </section>
+        <section>
+          <h2>Privacy controls</h2>
+          <p>Privacy controls in iOS 8 and later give you control over which apps have access to information stored on your iOS device. For example, you can allow a social-networking app to use your camera, allowing you to take and upload pictures. You can also grant access to your contacts, so a messaging app can find any friends that are already using the same app.</p>
+          <img src="https://placeimg.com/640/350/tech" />
 
-        <p>{JSON.stringify(this.props)}</p>
+          <p>You can modify privacy settings in Settings > Privacy. You can select a type of data from this list to see which apps have asked for permission to use that data. An app won't appear on the list until it asks permission to use your data. You can add or remove permission from any app that has asked for access to data. An app can use your data only if you have given it your permission.</p>
+
+          <p>If you allow third-party apps or websites to use your data or your current location, you're subject to their terms, privacy policies, and practices. You should review the terms, privacy policies, and practices of the apps and websites to understand how they use your location and other information. Information Apple collects will be treated in accordance with Apple's Privacy Policy.</p>
+          <img src="https://placeimg.com/640/380/tech" />
+
+        </section>
+
       </div>
+    </div>
+
     );
   }
 }
