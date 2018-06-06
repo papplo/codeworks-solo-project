7e6fe385f383e33dc40e02c3ce0dfa68b0ac1071
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-06 16:37:49 +0200

    feat: redux now visible in devtools

--- client/src/store.js
+++ client/src/store.js
@@ -6,6 +6,7 @@ import rootReducer from './reducers/rootReducer';
 export default function configureStore(initialState={}) {
   return createStore(
     rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
   );
 }
