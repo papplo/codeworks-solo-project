80acd2eba4f64ac2f67f6315ba6fe5f4e68ee746
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-08 16:45:04 +0200

    feat: styled components now take props as arguments for styling FTW

--- client/src/components-styled/buttons/Button.js
+++ client/src/components-styled/buttons/Button.js
@@ -1,14 +1,30 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+
+  /* Adapt the colours based on color prop */
+  background: ${
+    props => props.color ?
+    'transparent' : 'white'};
+
+  color: ${
+    props => props.color ?
+    props.color : 'white'};
+
+  border: 2px solid ${
+    props => props.color ?
+    props.color : 'white'};
+
+  font-size: ${
+    props => props.size ?
+    '1.15em' : '.9em'};
+
+
+
   border-radius: 3px;
   padding: 0.25em 1em;
-  font-size: .9em;
-  background: transparent;
-  color: white;
-  border: 2px solid white;
-  -webkit-tap-highlight-color: rgba(255,255,255,0.2);
-  tap-highlight-color: rgba(255,255,255,0.2);
+  -webkit-tap-highlight-color: rgba(255,255,255,0.4);
+  tap-highlight-color: rgba(255,255,255,0.4);
   outline: 0;
 
 `;
