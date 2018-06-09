e6b3dd943bfb0d80267df865d1e8da2cf33dc044
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-09 14:05:44 +0200

    feat: usersController updated, now hashing password with bcrypt and doing some basic error-checking

--- server/src/controller/users.controller.js
+++ server/src/controller/users.controller.js
@@ -1,22 +1,50 @@
-
 // C R /U /D : All but working example, time to commit it!
 const TellusUser = require('../model/user');
 
-const createUser = (req, res) => {
+const jwt = require('jsonwebtoken');
 
-  if (req.body.email && req.body.passwordHash && req.body.username) {
+const bcrypt = require('bcrypt');
+const saltRd = 10;
+
+const countUsers = async () => {
+  await TellusUser.count()
+    .then(c => console.log('Users in db: ' +c));
+}
+
+const createUser = async (req, res) => {
+
+  // catch and check params
+  const {username, password, email} = req.body;
+  if (username && password && email) {
+
+    // check if name exists
+    const check = await TellusUser.findOne({'username': username});
+    if (check) {
+      console.log(req.body, ' <- Request.body. ERR USER Exists');
+      res.status(400).send({ error: "User already exists!" });
+      res.end()
+    }
+
+
+    // pseudo: create passwordHash
+    let hash = '';
+    await bcrypt.hash(password, saltRd).then((h) => (hash = h));
+    // store as object and pass to model
+    // create token
+
     const user = new TellusUser({
-      'passwordHash': req.body.passwordHash,
-      'email'       : req.body.email,
-      'username'    : req.body.username,
-      'tokenSeed'   : req.body.tokenSeed,
+      'passwordHash': hash,
+      'email'       : email,
+      'username'    : username,
+      'tokenSeed'   : ' ',
     })
     user.save()
       .then(event => res.json(event))
+      .then(countUsers())
       .catch(e => console.log(e))
     }
   else {
-    console.log('Prob Somethings wrong');
+    console.log(req.body, ' <- Request.body. Prob Somethings wrong');
     res.status(500).send({ error: "Something is missing in your post!" });
     res.end()
   }
