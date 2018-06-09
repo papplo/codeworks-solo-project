6b5e08a7c375efb1b78965e0c27672d550ac6520
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-09 12:59:32 +0200

    feat: digging into middleware in express, starting with debug and cors settings.

--- server/src/server.js
+++ server/src/server.js
@@ -5,34 +5,36 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const dotenv = require('dotenv');
 const router = require('./route/auth-router');
 const app = express();
 
+/* middleware here */
+const {corsOptions} = require('./middleware/CorsOptions');
+
 // our .env variables
+dotenv.config();
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/cw-soloproject';
 
+// our db connect instance
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
+
 /* Here we go */
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(cors());
-
-/* middleware here */
 app.use('/', router);
-
-
 
 const start = () => {
   app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+    console.log(`CORS server listening on port: ${PORT}`);
   })
 }
 const stop = () => {
   app.close(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+    console.log(`CORS server STOPPED on port: ${PORT}`);
   })
 }
 
